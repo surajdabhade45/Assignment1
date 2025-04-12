0aba7e94c0bfa4cc751f3cb0f49e6570f9a805fb
surajdabhade45 <116947563+surajdabhade45@users.noreply.github.com>
2025-04-13 03:58:45 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,15 @@
+#Task1
+p=input('Enter the first number: ')
+p=int(p)
+q=input('Enter the second number: ')
+q=int(q)
+
+a=p+q
+s=p-q
+m=p*q
+d=p/q
+
+print("Addition: ", a)
+print("Subtraction: ", s)
+print("Multiplication: ", m)
+print("Division: ", d )
---
+++ Task2.py
@@ -0,0 +1,9 @@
+#Task2
+
+a=input('Enter your first name: ')
+
+b=input('Enter your last name:  ')
+
+c=b+'!'
+
+print('Hello,', a + " " + c, 'Welcome to the python program')
